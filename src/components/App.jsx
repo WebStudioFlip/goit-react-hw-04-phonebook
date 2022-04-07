fa01e55190c5e4c399b7249dd56337d8e095ff3b
WebStudioFlip <73443296+WebStudioFlip@users.noreply.github.com>
2022-04-08 00:26:53 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,60 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 import Section from '../shared/Section';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+  return  JSON.parse(localStorage.getItem('contacts')) ?? [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]
+  } );
+  const [filter, setFilter] = useState('');  
+  
+  useEffect (()=>{          
+      localStorage.setItem('contacts', JSON.stringify(contacts));   
+  },[contacts])
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    if (contacts && JSON.parse(contacts).length) {
-      this.setState({
-        contacts: JSON.parse(contacts),
-      });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      const { contacts } = this.state;
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
-
-  addContact = addContact => {
-    const { contacts } = this.state;
+  const addContact = addContact => {    
+    const {name} = addContact;
     if (
       !contacts.find(
-        el => el.name.toLowerCase() === addContact.name.toLowerCase()
+        el => el.name.toLowerCase() === name.toLowerCase()
       )
     ) {
       addContact.id = nanoid();
-      this.setState(() => {
-        return { contacts: [...contacts, addContact] };
+      setContacts(() => {
+        return [...contacts, addContact] 
       });
     } else {
-      alert(`${addContact.name} is already in contacts`);
+      alert(`${name} is already in contacts`);
     }
   };
 
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
+  const getFilteredContacts = () => {    
     if (!filter) {
       return contacts;
     }
     const filterStr = filter.toLowerCase();
-    const result = contacts.filter(contact => {
-      const name = contact.name.toLowerCase();
-      return name.includes(filterStr);
-    });
+    const result = contacts.filter(({name}) => name.toLowerCase().includes(filterStr));
     return result;
   };
 
-  handleSearch = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
+  const handleSearch = e => {
+    const { value } = e.target;
+    setFilter(value);
   };
 
-  removeContact = contactId => {
-    this.setState(prevState => {
-      const { contacts } = prevState;
-      const newContacts = contacts.filter(item => item.id !== contactId);
-      return {
-        contacts: newContacts,
-      };
-    });
+  const removeContact = contactId => {
+    setContacts(contacts.filter(item => item.id !== contactId));
   };
 
-  render() {
+  
     return (
       <div
         style={{
@@ -94,21 +70,20 @@ class App extends Component {
         }}
       >
         <Section title="Phonebook">
-          <ContactForm addContact={this.addContact} />
+          <ContactForm addContact={addContact} />
         </Section>
         <Section title="Contacts">
           <Filter
-            filterContacts={this.handleSearch}
-            filter={this.state.filter}
+            filterContacts={handleSearch}
+            filter={filter}
           />
           <ContactList
-            contacts={this.getFilteredContacts()}
-            removeContact={this.removeContact}
+            contacts={getFilteredContacts()}
+            removeContact={removeContact}
           />
         </Section>
       </div>
-    );
-  }
+    );  
 }
 
 export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,77 +1,76 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import style from './contactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const ContactForm = ({ addContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = event => {
+  const handleChange = event => {
     const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleFormSubmit = event => {
+  const handleFormSubmit = event => {
     event.preventDefault();
-    const {addContact} = this.props;    
-    addContact(this.state);
-    this.setState({
-      name: '',
-      number: '',
-    });
+    addContact({ name, number });
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <form className={style.form} onSubmit={this.handleFormSubmit}>
-        <div className={style.formGroup}>
-          <label htmlFor="" className={style.formGroupLabel}>
-            Name
-          </label>
-          <input
-            className={style.formGroupInput}
-            onChange={this.handleChange}
-            value={name}
-            type="text"
-            name="name"
-            placeholder="Enter Name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </div>
-        <div className={style.formGroup}>
-          <label htmlFor="" className={style.formGroupLabel}>
-            Number
-          </label>
-          <input
-            type="tel"
-            value={number}
-            className={style.formGroupInput}
-            onChange={this.handleChange}
-            placeholder="Enter Phone"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </div>
-        <div className={style.formGroup}>
-          <label htmlFor="" className={style.formGroupLabel}></label>
-          <button className={style.btn} type="submit">
-            Добавить
-          </button>
-        </div>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={style.form} onSubmit={handleFormSubmit}>
+      <div className={style.formGroup}>
+        <label htmlFor="" className={style.formGroupLabel}>
+          Name
+        </label>
+        <input
+          className={style.formGroupInput}
+          onChange={handleChange}
+          value={name}
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+      <div className={style.formGroup}>
+        <label htmlFor="" className={style.formGroupLabel}>
+          Number
+        </label>
+        <input
+          type="tel"
+          value={number}
+          className={style.formGroupInput}
+          onChange={handleChange}
+          placeholder="Enter Phone"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </div>
+      <div className={style.formGroup}>
+        <label htmlFor="" className={style.formGroupLabel}></label>
+        <button className={style.btn} type="submit">
+          Добавить
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default ContactForm;
 
